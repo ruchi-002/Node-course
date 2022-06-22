cd18e8d15a4639c516f3c5c00251ca007f44f21d
Ruchika sharma <ruchikas@geekyants.com>
2022-06-22 16:46:53 +0530

    packages

--- node-pratice/index.js
+++ node-pratice/index.js
@@ -1,6 +1,25 @@
-const fs=require('fs');
+// const fs=require('fs').writeFileSync;
+// fs("abc.txt","abc");
+  
+const http=require('http');
 
-fs.writeFileSync("Hello.txt",'Hello GeekyAnts Member');
-console.log("Hey!!");
-console.log(__dirname);
-console.log(__filename);
+// http.createServer((req,res)=>{
+//     // res.write("Hello");
+//     res.write('<h1>Hey!!</h1>');
+//     res.end();
+
+// }).listen(8080)
+
+// function dataControl(req,res){
+//     res.write("<h1>Hello this is ruchika sharma </h1>");
+//     res.end();
+// }
+// const dataControl=(req,res)=>{
+//     res.write("<h1>Hello ,  this is ruchika sharma </h1>");
+//     res.end();
+// }
+
+// http.createServer(dataControl).listen(8000);
+
+const colors=require('colors');
+console.log("hello".blue);
