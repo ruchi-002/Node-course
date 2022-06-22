9336cf2446b366ab89c618fa89414053100bbf79
Ruchika sharma <ruchikas@geekyants.com>
2022-06-22 15:06:04 +0530

    import and export

---
+++ node-pratice/add.js
@@ -0,0 +1,6 @@
+exports.add=function(a,b){
+  return a+b;
+}
+exports.multiply=function(a,b){
+    return a*b;
+}
--- node-pratice/index.js
+++ node-pratice/index.js
@@ -1,10 +1,17 @@
-// console.log("Hello world");
-var http=require('http');
-http.createServer(function(req,res){
-    
-    // res.write('Welcome in GeekyAnts');
-    res.writeHead(200,{'Content-Type':'text/html'})
-    res.end("Welcome in GeekyAnts");
-    // console.log("Welcome in GeekyAnts");
-    
-}).listen(8080);
+const app=require('./app');
+console.log("Hey!!"); //hey
+console.log(app);// app function
+console.log(app.x);// value
+console.log(app.y); // value
+console.log(app.z()); // value
+
+const add=require('./add');
+console.log(add.add(2,3));//5
+console.log(add.multiply(3,4));//12
+
+const arr=[1,2,3,4,5,6,7];
+arr.filter((item)=>{
+    console.log(item); // 1,2,3,4,5,6,7
+})
+
+
