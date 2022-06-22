c484934ed4448981ac811e2e4e4c20a92f3f2fd3
Ruchika sharma <ruchikas@geekyants.com>
2022-06-22 15:50:06 +0530

    fs /core module

--- node-pratice/index.js
+++ node-pratice/index.js
@@ -1,12 +1,6 @@
-var http=require('http');
-http.createServer(function(req,res){
-    // send the HTTP header
-    // HTTP status 200 ok
-    // Content-type:text/plain
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    // send the response body as "Welcome in GeekyAnts"
-    res.end('welcome in GeekyAnts');
-}).listen(8000);
-// console will print the message 
-console.log('server is running on port : 8000');
+const fs=require('fs');
 
+fs.writeFileSync("Hello.txt",'Hello GeekyAnts Member');
+console.log("Hey!!");
+console.log(__dirname);
+console.log(__filename);
