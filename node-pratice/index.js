dbb1fb0f29420cda715af5b7bab01b44081c3ddd
Ruchika sharma <ruchikas@geekyants.com>
2022-06-22 19:31:11 +0530

    nodejs file system assignment

--- node-pratice/index.js
+++ node-pratice/index.js
@@ -1,7 +1,13 @@
 var fs=require('fs');
-fs.unlink('hello.txt',function(){
-    console.log("Deleted successfully!");
+fs.writeFile('MyBlog.txt',' am glad to welcome you on my website devoted to traveling. My name is Uliana, I am 30 years old and as you may guess I am fond of exploring this big and incredible world. I was born in Kiev, Ukraine and I am living and working there between my travels. I am sorry for my English which is far away from perfect, but I hope it will not disturb you much and you will find my articles and stories interesting and informative',function(err){
+    if(err)
+    console.log(err);
+    else 
+    console.log('write operation successfully!!');
 });
-fs.unlink('TestFile.txt',function(){
-    console.log("Deleted operation completed");
-})
+fs.readFile('MyBlog.txt',function(err,data){
+    if(err)
+    throw err;
+
+    console.log(data);
+});
