bc4c55b48a3d097b7c6884c7baf98039adac0ad6
Ruchika sharma <ruchikas@geekyants.com>
2022-06-22 19:06:57 +0530

    reading file through fs method

--- node-pratice/index.js
+++ node-pratice/index.js
@@ -1,25 +1,6 @@
-// const fs=require('fs').writeFileSync;
-// fs("abc.txt","abc");
-  
-const http=require('http');
-
-// http.createServer((req,res)=>{
-//     // res.write("Hello");
-//     res.write('<h1>Hey!!</h1>');
-//     res.end();
-
-// }).listen(8080)
-
-// function dataControl(req,res){
-//     res.write("<h1>Hello this is ruchika sharma </h1>");
-//     res.end();
-// }
-// const dataControl=(req,res)=>{
-//     res.write("<h1>Hello ,  this is ruchika sharma </h1>");
-//     res.end();
-// }
-
-// http.createServer(dataControl).listen(8000);
-
-const colors=require('colors');
-console.log("hello".blue);
+var fs=require('fs');
+fs.readFile('TestFile.txt',function(err,data){
+    if(err)
+    throw err;
+    console.log(data);
+});
