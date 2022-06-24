7d38d16ca862d5331d2a3e0f69f7c23d82807a01
Ruchika sharma <ruchikas@geekyants.com>
2022-06-24 14:42:03 +0530

    middleware assignment

--- middleware-folder/index.js
+++ middleware-folder/index.js
@@ -2,9 +2,9 @@ var express=require('express');
 var app=express();
 app.use(function(req,res){
     console.log("A new request recieved at " + Date.now());
-    next();
+    // next();
 });
 app.listen(3000,(req,res)=>{
-    console.log("server is running on port ");
+    console.log("server is running on port:3000 ");
 
 })
