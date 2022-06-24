fb912fbd4dc5482e21395f7f4ffcfde50ca62649
Ruchika sharma <ruchikas@geekyants.com>
2022-06-24 14:32:25 +0530

    middleware

---
+++ middleware-folder/index.js
@@ -0,0 +1,10 @@
+var express=require('express');
+var app=express();
+app.use(function(req,res){
+    console.log("A new request recieved at " + Date.now());
+    next();
+});
+app.listen(3000,(req,res)=>{
+    console.log("server is running on port ");
+
+})
