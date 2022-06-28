d7795ba8085dadc5f3384b86c9b3ddfd734cb3bd
Ruchika sharma <ruchikas@geekyants.com>
2022-06-28 18:58:29 +0530

    registration form completed

--- assignment-registration-form/index.js
+++ assignment-registration-form/index.js
@@ -1,14 +1,47 @@
 const express=require('express');
 const port=8000;
 const app=express();
+const path=require('path');
 const mongoose=require('mongoose');
 const bodyParser=require('body-parser');
+app.use(bodyParser.json());
 // set
 app.set("view engine","ejs");
+mongoose.connect('mongodb://localhost:27017/UserInfo',{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+   
+});
+var db=mongoose.connection;
+db.on('error',console.log.bind(console,"connection error"))
+db.once('open',function(callback){
+    console.log("connection succeeded");
+})
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.get('/',(req,res)=>{
     res.render('form');
 });
-app.listen(port,(req,res)=>{
-    console.log(`server is running on port ${port}`);
+app.post('/list',function(req,res){
+    var user_name=req.body.user_name;
+    var user_email=req.body.user_email;
+    var user_phone=req.body.user_phone;
+    var user_address=req.body.user_address;
+    var data={
+        "user_name":user_name,
+        "user_email":user_email,
+        "user_phone":user_phone,
+        "user_address":user_address
+    }
+    db.collection("UserDetails").insertOne(data,function(err,collection){
+        if(err)
+        throw err;
+        console.log("Record inserted");
+    })
+    res.send("inserted")
+  
+})
+
+app.listen("3000",(req,res)=>{
+    console.log(`server is running on port 3000`);
 })
